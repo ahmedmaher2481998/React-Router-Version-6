558b7e5ecca60a848edc48a85cb98d75edf50af7
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-05-05 01:43:37 +0200

    upgraded succesfully

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import ProductDetails from './pages/ProductDetails'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>The Ninja Clothing Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<nav>
+					<h1>The Ninja Clothing Company</h1>
+					<Link to='/'>Home</Link>
+					<Link to='/about'>About</Link>
+					<Link to='/products'>Products</Link>
+				</nav>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/about/*' element={<About />} />
+					<Route
+						path='/products/:id/*'
+						element={<ProductDetails />}
+					/>
+					<Route path='/products' element={<Products />} />
+
+					{/* ************V5********************** */}
+					{/* <Route exact path='/' component={Home} /> */}
+					{/* <Route path='/about'>
+						<About />
+					</Route> */}
+					{/* <Route path='/products/:id'>
+						<ProductDetails />
+					</Route> */}
+					{/* <Route path='/products'>
+						<Products />
+					</Route> */}
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,46 @@
-import { Route } from "react-router-dom"
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 // nested routes
-import Offers from "./Offers"
+import Offers from "./Offers";
+import Products from "./Products";
 
 export default function About() {
-  return (
-    <div className="content">
-      <h2>About Us</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
-      
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+	const redirect = true;
+	return (
+		<div className='content'>
+			<h2>About Us</h2>
+			<Link to={"offers"}>
+				<button>offers</button>
+			</Link>
 
-      <Route path="/about/offers">
-        <Offers />
-      </Route>
-    </div>
-  )
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+				tempora nisi eligendi quod ut illo cumque quibusdam
+				reprehenderit veniam. Cum eaque, nostrum fugit ut inventore
+				assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor
+				sit amet consectetur adipisicing elit. Obcaecati nam repellat
+				hic dolore officia sequi quis quod commodi iusto doloremque.
+				Corporis in et placeat unde sapiente perspiciatis minus!
+				Recusandae, ad!
+			</p>
+
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+				tempora nisi eligendi quod ut illo cumque quibusdam
+				reprehenderit veniam. Cum eaque, nostrum fugit ut inventore
+				assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor
+				sit amet consectetur adipisicing elit. Obcaecati nam repellat
+				hic dolore officia sequi quis quod commodi iusto doloremque.
+				Corporis in et placeat unde sapiente perspiciatis minus!
+				Recusandae, ad!
+			</p>
+			<Routes>
+				<Route path='offers' element={<Offers />} />
+				<Route
+					path='r'
+					element={<Navigate to={redirect ? "/products" : "/"} />}
+				/>
+			</Routes>
+		</div>
+	);
 }
